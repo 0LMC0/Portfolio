7f422955d5e9349dacb5c7577a7fe6b25e52ffbe
Lautaro <lautycaceresss871@gmail.com>
2022-04-22 18:25:39 -0300

    Cv download button and some fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,3 +12,13 @@ toggle.addEventListener('click', () =>{
 
   englishSpanish.textContent = toggle.checked ? "Mi nombre es Lautaro, soy una persona bilingúe de 22 años originaria de Argentina, graduado de un curso integro dictado por Coderhouse(HTML5, CSS3, Javascript y React.js). Trabajador dedicado con el objetivo de ayudar a lograr los objetivos y asumir más responsabilidades lo más rápido posible. Buscando aprovechar los conocimientos adquiridos (académicos y autodidactas) para cubrir efectivamente su puesto vacante." : "I'm Lautaro, a Bilingüal 22 year old originally from Argentina Graduated from a complete Front-end course dictated by Coderhouse(HTML5, CSS3, Javascript,React.js). Dedicated worker aiming to help achieve objectives and take on more responsability as quickly as possible!. Seekin to leverage adquired (Academic and Self-learned) knowlegde to effectively fill your open job position.";
 })
+
+// btn cv download
+
+
+
+const btnCv= document.querySelector('.btn-custom-cv');
+
+btnCv.addEventListener('click', function handleClick(event) {
+  event.target.classList.add('active-cv');
+});
